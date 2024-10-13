29d68c1b5499d8e1baf292e1be44c4b44748bb7b
Marlon <frazao.marlon@gmail.com>
2024-10-13 10:33:14 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,16 @@
+import { InMemoryDbService, RequestInfo } from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService {
+    createDb() {
+        const categories = [
+            { id: 1, name: "Moradia", description: "Contas da Casa" },
+            { id: 2, name: "Saúde", description: "Pleano de Saúde e Remédios" },
+            { id: 3, name: "Lazer", description: "Cinema, parques, praias, etc" },
+            { id: 4, name: "Salário", description: "Recebimento de Salário" },
+            { id: 5, name: "Freelas", description: "Trabalhos como freelancer" },
+            { id: 6, name: "Música", description: "Gastos e Recebimentos com as Bandas" }
+        ];
+
+        return { categories }
+    }
+}
